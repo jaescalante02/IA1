6d632d73d78a919b0755f847eae5c4a5b4bd5196
Gamar <gamarazuaje@gmail.com>
2014-04-27 18:10:33 -0430

    Continuado pdb

--- codigos/pdb.h
+++ codigos/pdb.h
@@ -1,12 +1,76 @@
 #ifndef IA_PDB
 #define IA_PDB
 
+#include <vector>
+#include <iterator>
+#include <stdio.h>
 #include "heuristics.h"
 
 
-unsigned long long acum[7];
 
-int k(unsigned long long arg[],int pos){
+
+//Functions to travel to state
+
+unsigned long long goalState(){
+	unsigned long long g=0ULL;
+	for(unsigned long long i=0ULL;i<16ULL;i++) g += (i<<(4*(15-i)));
+	return g;
+}
+
+inline unsigned long long vecino(unsigned long long state, int ind, int vec, int mov){
+		unsigned long long tmp = state & (15ULL<<(4*ind));
+		unsigned long long tmp2 = state & (15ULL<<(4*vec));
+		unsigned long long tmp3 = state - tmp - tmp2;
+		tmp>>=mov;
+		tmp2<<=mov;
+		return tmp3 + tmp + tmp2;
+
+}
+
+vector<unsigned long long> next(unsigned long long state){
+	int pos = -1;
+	for(int i=0;i<16 && pos ==-1;i++)
+		if(!(state & (15ULL<<(4*i)))) pos = i;
+	
+	vector<unsigned long long> ret;
+
+	if(pos % 4)
+		ret.push_back(vecino(state,pos,pos-1,4));
+	if(pos % 4 != 3)
+		ret.push_back(vecino(state,pos+1,pos,4));
+	if(pos>3)
+		ret.push_back(vecino(state,pos,pos-4,16));
+	if(pos<12)
+		ret.push_back(vecino(state,pos+4,pos,16));
+
+	return ret;
+}
+
+
+
+class pdbHash{
+  public:
+  int size; //Size of pattern
+  int start; //
+  vector<int> acum;
+  pdbHash(int st, int s){
+    start = st;
+    size = s;
+    acumulados();
+  }
+  
+  void acumulados(){
+  int acumAux[size];
+  acumAux[size-1] = 1;
+  int aux = start - (size - 1);
+  for(int i=size-2;i>=0;i--){  
+    acumAux[i] = acumAux[i+1]*aux;
+    aux++;
+  }
+  acum = vector<int>(acumAux,acumAux + sizeof acumAux/ sizeof acumAux[0]);
+ }
+
+  int k(int arg[],int pos){
 
   int mayor = 0;
   for(int i=0;i<pos;i++) 
@@ -14,68 +78,39 @@ int k(unsigned long long arg[],int pos){
 
   return mayor;
 
-}
-
-void acumulados(){
-
-  acum[6] = 1ULL;
-  unsigned long long aux = 10ULL;
-  for(int i=5;i>=0;i--){
-  
-  acum[i] = acum[i+1]*aux;
-  aux++;
-  
   }
 
-}
-
-void arguments(unsigned long long arg[], NODO n){
-
-	int left=0;
-	unsigned long long right=15ULL;
-  for(int i=15;i>=0;i--){
-  
-    unsigned long long tmp = n.state & right;
-		tmp>>=left;
-	  right<<=4;
-		left+=4;        
-    if((tmp<8ULL) && (tmp>0ULL)) arg[tmp-1ULL] = i;
-  
-  }
-
-}
-
-/*
-16*15*14*13*12*11*10 = 58 millones 
-
-f(a0,a1,a2,a3,a4,a5,a6) = (a0-k0)*15*14*13*12*11*10 +
-                          (a1-k1)*14*13*12*11*10+
-                          (a2-k2)*13*12*11*10+
-                          .
-                          .
-                          .
-                          (a5-k5)*10+
-                          (a6-k6)
-
-*/
-
-unsigned long long int hash_15pdb_one(NODO n){
-
-  unsigned long long arg[7];
-  arguments(arg,n);
-  for(int i=0;i<7;i++)
-   cout<<arg[i]<<" ";
-    for(int i=0;i<7;i++)
-   cout<<acum[i]<<" "; 
-  unsigned long long index=0ULL;
-  for(int i=0;i<7;i++)
+  int hash(int arg[]){
+  int index=0;
+  for(int i=0;i<size;i++)
     index += (arg[i]-k(arg,i))*acum[i];
 
-return index;
-
+  return index;
 }
 
+  void print(){
+    for (vector<int>::const_iterator i = acum.begin(); i != acum.end(); i++){
+      std::cout << *i << "\n";
+    }
+  }
+};
 
 
+//Pattern for 15 puzzle, size 8.
+void pattern15_8(int arg[], unsigned long long n){
+	for (int i = 7; i >= 0; i--){
+	  arg[i] = n & 15;
+	  n>>=4;
+	}
+}
+
+//Pattern for 15 puzzle, size 7.
+void pattern15_7(int arg[], unsigned long long n){
+  n>>=32;
+	for (int i = 6; i >= 0; i--){
+	  arg[i] = n  & 15;
+	  n>>=4;
+	}
+}
 
 #endif
