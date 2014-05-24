2d049868e31b77b31c21fba902a7fd32aa5f5e01
Wilmer Bandres <wilmer0593@hotmail.com>
2014-05-24 01:04:55 -0430

    agregando scout, no se por que no da el fucking scout el valor que deberia

--- P2/codigos/main.cc
+++ P2/codigos/main.cc
@@ -20,10 +20,11 @@
 
 #include "othello_cut.h" // won't work correctly until .h is fixed!
 #include "minimax.h"
+#include "scout.h"
 #include <iostream>
 
-const int PASOS = 2; //Define la cantidad de pasos a hacer por la variacion principal
-					  //para luego empezar a ejecutrar los algoritmos
+const int PASOS = 3; //Define el nivel desde las hojas de donde se 
+					 //va a empezar a ejecutar los algoritmos
 					  //Debe ser un numero par para que la proxima jugada sea
 					  //de las fichas negras que es con respecto a quien se 
 					  //calculan los algoritmos
@@ -32,7 +33,7 @@ using namespace std;
 
 int main(int argc, const char **argv) {
     state_t state,state2;
-	int cnt = PASOS;
+	int cnt = 32-PASOS;
     cout << "Principal variation:" << endl;
     for( int i = 0; PV[i] != -1; ++i ) {
         bool player = i % 2 == 0; // black moves first!
@@ -53,7 +54,7 @@ int main(int argc, const char **argv) {
 	cout << state2 << endl;
     
     
-    if( argc > 1 ) {
+    /*if( argc > 1 ) {
         int n = atoi(argv[1]);
         cout << endl << "Apply " << n << " random movements at empty board:";
         state = state_t();
@@ -67,12 +68,21 @@ int main(int argc, const char **argv) {
             state = state.move(player, pos);
             cout << " " << pos;
         }
-        bool player = n%2 == 0;
+        bool player = true;
         cout << endl << "Minimax value " << minimax(state,PASOS,player);
-        cout << " Negamax value " << negamax(state,PASOS,player);
-        cout << " Alpha-Beta value " << alphabeta(state,PASOS,player) << endl;
+        cout << " Negamax value " << negamax(state2,PASOS,player);
+        cout << " Alpha-Beta value " << alphabeta(state2,PASOS,player) << endl;
+		cout << "scout value " << scout(state2,PASOS,player) << endl;
         cout << endl << state;
     }
+*/
+	bool player = true;
+     /*   cout << endl << "Minimax value " << minimax(state,PASOS,player);
+        cout << " Negamax value " << negamax(state2,PASOS,player);
+        cout << " Alpha-Beta value " << alphabeta(state2,PASOS,player) << endl;
+	*/	cout << "scout value " << scout(state2,PASOS,player) << endl;
+        cout << endl << state;
+
 
     return 0;
 }
---
+++ P2/codigos/scout.h
@@ -0,0 +1,44 @@
+#include<cstdio>
+
+using namespace std;
+
+#ifndef SCOUT
+#define	SCOUT
+
+//maxi es true si es un nodo de maximizacion y false si es de minimizacion
+//st es el estado a verificar en la funcion
+bool test(state_t &st, int depth, int v, bool maxi){
+	if(!depth || st.terminal()) return st.value()>v;
+	vector<int> vect = st.valid_moves(maxi);
+	state_t child;
+
+	if(!vect.size()) return test(st,depth,v,!maxi);
+	
+	for(int i=0;i<vect.size();i++)
+		if((&(child = st.move(maxi,vect[i]))!=NULL) && test(child,depth-1,v,maxi))
+			return maxi;
+	
+	return !maxi;
+}
+
+
+//maxi es similar a la funcion anterior
+int scout(state_t &st, int depth, bool maxi){
+	if(!depth || st.terminal()) return st.value();
+	vector<int> v = st.valid_moves(maxi);
+	if(!v.size()) return scout(st,depth,!maxi);
+	state_t child;
+	child = st.move(maxi,v[0]);
+	int val = scout(child,depth-1,maxi);
+	for(int i=1;i<v.size();i++)
+		if((&(child = st.move(maxi,v[i]))!=NULL) && (test(child,depth-1,val,maxi))==maxi)
+			val = scout(child,depth-1,!maxi);
+	
+
+	return val;
+}
+
+	
+
+
+#endif
